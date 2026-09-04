fb434185a06c783ebac8d5dbccd1a97434d73faa
Rishabh <rishabh.chhangani@gmail.com>
2026-09-04 19:24:53 +0530

    refactor: Paddle Architecture

--- Assets/Scripts/PaddleScript/Paddle.cs
+++ Assets/Scripts/PaddleScript/Paddle.cs
@@ -6,10 +6,13 @@ public class Paddle : MonoBehaviour
     public float speed = 10.0f;
     protected Rigidbody2D _rigidbody;
 
+    
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
+
 
     public void ResetPosition()
     {
